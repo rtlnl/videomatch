7b125d19f6aecadb427fc08e6ede5c7d2c89b100
Daan Odijk <Daan.Odijk@rtl.nl>
2022-10-04 21:03:00 +0200

    Do not cache examples anymore.

--- app.py
+++ app.py
@@ -91,17 +91,13 @@ with open('apb2022.json', "r") as json_file:
     TARGET_ENTRIES = json.load(json_file)
 
 EXAMPLE_VIDEO_URLS = ["https://drive.google.com/uc?id=1Y1-ypXOvLrp1x0cjAe_hMobCEdA0UbEo&export=download",
-                    "https://video.twimg.com/amplify_video/1575576025651617796/vid/480x852/jP057nPfPJSUM0kR.mp4?tag=14",
-                    "https://www.dropbox.com/s/8c89a9aba0w8gjg/Ploumen.mp4?dl=1",
-                    "https://www.dropbox.com/s/rzmicviu1fe740t/Bram%20van%20Ojik%20krijgt%20reprimande.mp4?dl=1",
-                    "https://www.dropbox.com/s/wcot34ldmb84071/Baudet%20ontmaskert%20Omtzigt_%20u%20bent%20door%20de%20mand%20gevallen%21.mp4?dl=1",
-                    "https://drive.google.com/uc?id=1XW0niHR1k09vPNv1cp6NvdGXe7FHJc1D&export=download",
-                    "https://www.dropbox.com/s/4ognq8lshcujk43/Plenaire_zaal_20200923132426_Omtzigt.mp4?dl=1"]
+                      "https://video.twimg.com/amplify_video/1575576025651617796/vid/480x852/jP057nPfPJSUM0kR.mp4?tag=14",
+                      "https://drive.google.com/uc?id=1XW0niHR1k09vPNv1cp6NvdGXe7FHJc1D&export=download"]
 
 index_iface = gr.Interface(fn=lambda url: index_hashes_for_video(url).ntotal, 
                      inputs="text", 
                      outputs="text", 
-                     examples=EXAMPLE_VIDEO_URLS, cache_examples=True)
+                     examples=EXAMPLE_VIDEO_URLS)
 
 # compare_iface = gr.Interface(fn=get_comparison,
 #                      inputs=["text", "text", gr.Slider(2, 30, 4, step=2)], 
