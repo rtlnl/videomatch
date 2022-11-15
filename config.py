6715214362013486b578b88f3c7721ddf1420cc7
Iskaj <iskajjanssen1@gmail.com>
2022-11-15 14:09:22 +0100

    add documentation to config.py

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import tempfile
 
+# Create a temporary directory where all the videos get stored
 VIDEO_DIRECTORY = tempfile.gettempdir()
-# VIDEO_DIRECTORY = './data/'
 
+# Frames per second, only take 5 frames per second to speed up processing. 
+# Quite standard for video processing applications. 
 FPS = 5
+
+# Min and maximum distance between hashes when searching the videos for matches.
 MIN_DISTANCE = 4
-MAX_DISTANCE = 50 # Used to be 30
+MAX_DISTANCE = 50
+
+# Rolling window size that is used when calculating the mode of the distances between the videos.
 ROLLING_WINDOW_SIZE = 10
