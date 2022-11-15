58b39ad25cb3aff69685941ec338a0e2b41cfd16
Iskaj <iskajjanssen1@gmail.com>
2022-11-15 15:20:48 +0100

    add doc to plot.py

--- plot.py
+++ plot.py
@@ -8,62 +8,33 @@ from scipy import stats as st
 
 from config import FPS
 
-
-def plot_comparison(lims, D, I, hash_vectors, MIN_DISTANCE = 3):
-    sns.set_theme()
-
-    x = [(lims[i+1]-lims[i]) * [i] for i in range(hash_vectors.shape[0])]
-    x = [i/FPS for j in x for i in j]
-    y = [i/FPS for i in I]
-    
-    # Create figure and dataframe to plot with sns
-    fig = plt.figure()
-    # plt.tight_layout()
-    df = pd.DataFrame(zip(x, y), columns = ['X', 'Y'])
-    g = sns.scatterplot(data=df, x='X', y='Y', s=2*(1-D/(MIN_DISTANCE+1)), alpha=1-D/MIN_DISTANCE)
-
-    # Set x-labels to be more readable
-    x_locs, x_labels = plt.xticks() # Get original locations and labels for x ticks
-    x_labels = [time.strftime('%H:%M:%S', time.gmtime(x)) for x in x_locs]
-    plt.xticks(x_locs, x_labels)
-    plt.xticks(rotation=90)
-    plt.xlabel('Time in source video (H:M:S)')
-    plt.xlim(0, None)
-
-    # Set y-labels to be more readable
-    y_locs, y_labels = plt.yticks() # Get original locations and labels for x ticks
-    y_labels = [time.strftime('%H:%M:%S', time.gmtime(y)) for y in y_locs]
-    plt.yticks(y_locs, y_labels)
-    plt.ylabel('Time in target video (H:M:S)')
-
-    # Adjust padding to fit gradio
-    plt.subplots_adjust(bottom=0.25, left=0.20)
-    return fig 
-
-def plot_multi_comparison(df, change_points):
-    """ From the dataframe plot the current set of plots, where the bottom right is most indicative """
-    fig, ax_arr = plt.subplots(3, 2, figsize=(12, 6), dpi=100, sharex=True)
-    sns.scatterplot(data = df, x='time', y='SOURCE_S', ax=ax_arr[0,0])
-    sns.lineplot(data = df, x='time', y='SOURCE_LIP_S', ax=ax_arr[0,1])
-    sns.scatterplot(data = df, x='time', y='OFFSET', ax=ax_arr[1,0])
-    sns.lineplot(data = df, x='time', y='OFFSET_LIP', ax=ax_arr[1,1])
-
-    # Plot change point as lines 
-    sns.lineplot(data = df, x='time', y='OFFSET_LIP', ax=ax_arr[2,1])
-    for x in change_points:
-        cp_time = x.start_time
-        plt.vlines(x=cp_time, ymin=np.min(df['OFFSET_LIP']), ymax=np.max(df['OFFSET_LIP']), colors='red', lw=2)
-        rand_y_pos = np.random.uniform(low=np.min(df['OFFSET_LIP']), high=np.max(df['OFFSET_LIP']), size=None)
-        plt.text(x=cp_time, y=rand_y_pos, s=str(np.round(x.confidence, 2)), color='r', rotation=-0.0, fontsize=14)
-    plt.xticks(rotation=90)
-    return fig
-
 def change_points_to_segments(df, change_points):
-    """ Convert change points from kats detector to segment indicators """
+    """ Convert change points from kats detector to segment indicators. 
+    
+    Args:
+        df (DataFrame): Dataframe with information regarding the match between videos.
+        change_points ([TimeSeriesChangePoint]): Array of time series change point objects.
+    
+    Returns:
+        List of numpy.datetime64 objects where the first element is '0.0 in time' and the final element is the last
+        element of the video in time so the segment starts and ends in a logical place.
+    
+    """
     return [pd.to_datetime(0.0, unit='s').to_datetime64()] + [cp.start_time for cp in change_points] + [pd.to_datetime(df.iloc[-1]['TARGET_S'], unit='s').to_datetime64()]
 
 def add_seconds_to_datetime64(datetime64, seconds, subtract=False):
-    """Add or substract a number of seconds to a np.datetime64 object """
+    """ Add or substract a number of seconds to a numpy.datetime64 object. 
+    
+    Args: 
+        datetime64 (numpy.datetime64): Datetime object that we want to increase or decrease by number of seconds.
+        seconds (float): Amount of seconds we want to add or subtract.
+        subtract (bool): Toggle for subtracting or adding.
+
+    Returns:
+        A numpy.datetime64 object.
+
+    """
+    
     s, m = divmod(seconds, 1.0)
     if subtract:
         return datetime64 - np.timedelta64(int(s), 's') - np.timedelta64(int(m * 1000), 'ms')
@@ -74,12 +45,18 @@ def plot_segment_comparison(df, change_points, video_mp4 = "Placeholder.mp4", vi
     1. Make a decision on where each segment belongs in time and return that info as a list of dicts
     2. Plot how this decision got made as an informative plot
     
-    args:
-    - df: dataframe 
-    - change_points: detected points in time where the average metric value changes
-    - video_id: the unique identifier for the video currently being compared
-    - threshold_diff: to plot which segments are likely bad matches
+    Args:
+        df (DataFrame): Dataframe with information regarding the match between videos.
+        change_points ([TimeSeriesChangePoint]): Array of time series change point objects.
+        video_mp4 (str): Name of the source video to return as extra info.
+        video_id (str): The unique identifier for the video currently being compared
+        threshold_diff (float): Threshold for the average distance to plot which segments are likely bad matches.
+
+    Returns:
+        fig (Figure): Figure that shows the comparison between two videos.
+        segment_decisions (dict): JSON-style dictionary containing the decision information of the comparison between two videos.
     """
+    # Plot it with certain characteristics
     fig, ax_arr = plt.subplots(4, 1, figsize=(16, 6), dpi=300, sharex=True)
     ax_arr[0].set_title(video_id)
     sns.scatterplot(data = df, x='time', y='SOURCE_S', ax=ax_arr[0], label="SOURCE_S", color='blue', alpha=1.0)
@@ -146,11 +123,9 @@ def plot_segment_comparison(df, change_points, video_mp4 = "Placeholder.mp4", vi
                     "Source Video .mp4" : video_mp4,
                     "Uncertainty" : np.round(average_diff, 3),
                     "Average Offset in Seconds" : np.round(average_offset, 3),
-                    # "Explanation" : f"{start_time_str} -> {end_time_str} comes from video with ID={video_id} from {origin_start_time_str} -> {origin_end_time_str}",
                     }
         segment_decisions.append(decision) 
         seg_i += 1
-        # print(decision)
 
     # Return figure
     plt.xticks(rotation=90)
