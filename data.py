727e567c7f8a09dd0c9fafeec17eaeb88455e223
Iskaj <iskajjanssen1@gmail.com>
2022-11-15 15:36:47 +0100

    add documentation to data.py

--- data.py
+++ data.py
@@ -4,6 +4,9 @@ import shutil
 
 from videohash import filepath_from_url
 
+# < Algemene Politieke Beschouwing 2022 >
+# Load this data based on a .json file to get those videos to compare to.
+# This can be updated with any .json file containing other videos.
 with open('apb2022.json') as filein:
     urls, videos, url2video, video2url = [], [], {}, {}
     for item in json.load(filein):
@@ -12,13 +15,14 @@ with open('apb2022.json') as filein:
         url2video[item['url']] = item['mp4']
         video2url[item['mp4']] = item['url']
 
+# Get filepaths for the url's indices in the dataset and copy those to data folder if they're not present
 for url in videos:
     filepath = filepath_from_url(url) + '.index'
     datapath = os.path.join('data', os.path.basename(filepath))
     if not os.path.exists(filepath) and os.path.exists(datapath):
         shutil.copyfile(datapath, filepath)
 
-
+# To manually build the indices for the above dataset.
 if __name__ == "__main__":
     from videomatch import get_video_index
 
